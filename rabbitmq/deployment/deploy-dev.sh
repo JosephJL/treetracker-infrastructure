0e7834d831c86b59b81aa3717e76bd5f34fa987b
JosephJL <HeeJLJoseph@gmail.com>
2023-09-07 08:11:37 +1000

    Configure RabbitMQ

--- rabbitmq/deployment/deploy-dev.sh
+++ rabbitmq/deployment/deploy-dev.sh
@@ -1,2 +1,11 @@
-kubectl config use-context do-sfo2-dev-k8s-treetracker
+# kubectl config use-context do-sfo2-dev-k8s-treetracker
+# kustomize build overlays/dev | kubectl apply -n rabbitmq-cluster --wait -f -
+
+kubectl config use-context docker-desktop 
 kustomize build overlays/dev | kubectl apply -n rabbitmq-cluster --wait -f -
+
+# context -> point to digital ocean -> just need to set up a context for local environment 
+# when docker kubernetes is ready, can find a context insstalled by docker
+# kubectl config get-contexts -> get the kubernetes by docker with context name "docker-desktop"
+
+# need to find a namespace for rabbitmq, change the variables in deployment folder
